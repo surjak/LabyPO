c9551559ae69ff23fc458efedc1c1a65ead04747
surjak <surjak1999@gmail.com>
2019-10-20 15:10:06 +0200

    added ActionParser

--- PlanZajec/src/main/agh/cs/lab2/DeanerySystem.java
+++ PlanZajec/src/main/agh/cs/lab2/DeanerySystem.java
@@ -1,35 +1,39 @@
 package main.agh.cs.lab2;
 
+import main.agh.cs.lab3.ActionParser;
 import main.agh.cs.lab3.Lesson;
+
+import java.util.Arrays;
 
 public class DeanerySystem {
 
     public static void main(String[] args) {
-        Term term1 = new Term(9,45, Day.MON);
-        System.out.println(term1);
-        Term term2 = new Term(10,15, Day.MON);
-        System.out.println(term2);
-        System.out.println(term1.earlierThan(term2));
-        System.out.println(term1.equals(term2));
-        System.out.println(term1.endTerm(term2));
-        System.out.println(term1.endTerm());
-        Day day = Day.MON;
-        System.out.println(day);
-        System.out.println(day.prevDay().prevDay().prevDay().prevDay().prevDay().prevDay().prevDay().prevDay());
-        System.out.println(day.nextDay().nextDay().nextDay().nextDay().nextDay().nextDay().nextDay());
-        Lesson lesson = new Lesson(new Term(20,0,Day.FRI),"Programowanie  w języku Ruby","Stanisław Polak",2, false);
-        System.out.println(lesson);
-        System.out.println();
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
-        System.out.println(lesson.earlierTime());
 
+        String[] changeLesson = {"d+", "t+", "aaa", "dd", "d-", "t-"};
+        Action[] actions = ActionParser.parse(changeLesson);
 
-        System.out.println(lesson);
+        Lesson PO = new Lesson(new Term(10, 0, Day.WED), "Programowanie Obiektowe", "Stanisław Polak", 2, true);
+
+        Arrays.stream(actions).forEach(action -> {
+            switch (action) {
+                case DAY_LATER:
+                    PO.laterDay();
+                    System.out.println(PO);
+                    break;
+                case DAY_EARLIER:
+                    PO.earlierDay();
+                    System.out.println(PO);
+                    break;
+                case TIME_LATER:
+                    PO.laterTime();
+                    System.out.println(PO);
+                    break;
+                case TIME_EARLIER:
+                    PO.earlierTime();
+                    System.out.println(PO);
+                    break;
+            }
+        });
+        System.out.println(PO);
     }
 }
---
+++ PlanZajec/src/main/agh/cs/lab3/ActionParser.java
@@ -0,0 +1,28 @@
+package main.agh.cs.lab3;
+
+import main.agh.cs.lab2.Action;
+
+import java.util.Arrays;
+
+public class ActionParser {
+
+    private static Action parseOne(String a) {
+        switch (a) {
+            case "d-":
+                return Action.DAY_EARLIER;
+            case "d+":
+                return Action.DAY_LATER;
+            case "t-":
+                return Action.TIME_EARLIER;
+            case "t+":
+                return Action.TIME_LATER;
+            default:
+                return null;
+
+        }
+    }
+
+    public static Action[] parse(String[] args) {
+        return Arrays.stream(args).map(ActionParser::parseOne).filter(a -> a != null).toArray(Action[]::new);
+    }
+}
